09f309bc9a18f0137abbc7191a68a11bec161ccd
Vaibhavi Karn <76275888+vaibhavikarn2001@users.noreply.github.com>
2021-09-05 13:24:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(500,450)
+    canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('ImageWithfilter.png');
+}
